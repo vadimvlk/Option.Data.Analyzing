487c440c730627b1151de5af535351e3ce3e4f47
srvdev.volkov <v.volkov@globus.ru>
2025-05-11 20:04:09 +0300

    Add Quartz job scheduling to Scheduler project
    Integrated Quartz for job scheduling in the Scheduler project, including a new `DeribitJob` that runs every 3 hours. Updated dependencies, added necessary folder definitions, and configured the job and trigger in the application startup.

---
+++ Option.Data.Scheduler/Jobs/DeribitJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+
+namespace Option.Data.Scheduler.Jobs;
+
+[DisallowConcurrentExecution]
+public class DeribitJob: IJob
+{
+    private readonly ILogger<DeribitJob> _logger;
+
+    public DeribitJob(ILogger<DeribitJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Execute(IJobExecutionContext context)
+    {
+        _logger.LogInformation("DeribitJob executed at: {time}", DateTimeOffset.Now);
+        
+        return Task.CompletedTask;
+    }
+}
--- Option.Data.Scheduler/Program.cs
+++ Option.Data.Scheduler/Program.cs
@@ -1,5 +1,22 @@
-var builder = Host.CreateApplicationBuilder(args);
+using Quartz;
+using Option.Data.Scheduler.Jobs;
 
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-var host = builder.Build();
+builder.Services.AddQuartz(q =>
+{
+    // Register your job
+    JobKey jobKey = new JobKey("Deribit");
+    q.AddJob<DeribitJob>(opts => opts.WithIdentity(jobKey));
+    
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey)
+        .WithIdentity(nameof(DeribitJob))
+        .WithCronSchedule("0 0 0/3 * * ?")); // Run every 3 hours starting at 00:00 UTC
+
+});
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+IHost host = builder.Build();
 host.Run();
